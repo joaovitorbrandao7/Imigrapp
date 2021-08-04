37c051ed79db738e59629420adbe50367e613528
unknown <ba.amaral03@gmail.com>
2021-08-04 18:58:55 -0300

    [Task 6 -end-points e relacionamento]

--- Imigrapp/Imigrapp/src/main/java/org/generation/Imigrapp/Controller/UsuarioController.java
+++ Imigrapp/Imigrapp/src/main/java/org/generation/Imigrapp/Controller/UsuarioController.java
@@ -5,9 +5,15 @@ import java.util.List;
 import org.generation.Imigrapp.model.Usuario;
 import org.generation.Imigrapp.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +28,32 @@ public class UsuarioController {
 	
     @GetMapping
     public ResponseEntity<List<Usuario>> getAllUsuario(){
-    return ResponseEntity.ok(repository.findAll());
+    	return ResponseEntity.ok(repository.findAll()); 
+    }
+    
+    @GetMapping("/{id_usuario}")
+    public ResponseEntity<Usuario> getById(@PathVariable long id_usuario){
+    	return repository.findById(id_usuario).map(resp-> ResponseEntity.ok(resp))
+    			.orElse(ResponseEntity.notFound().build());
     }
 	
+    @GetMapping("/nome/{nomeUsuario}")
+    public ResponseEntity<List<Usuario>> getByNome (@PathVariable String nomeUsuario){
+    	return ResponseEntity.ok(repository.findAllByNomeUsuarioContainingIgnoreCase(nomeUsuario));
+    }
+    
+    @PostMapping
+    public ResponseEntity <Usuario> postUsuario (@RequestBody Usuario usuario){
+    	return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+    }
+    
+    @PutMapping
+    public ResponseEntity <Usuario> putUsuario (@RequestBody Usuario usuario){
+    	return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuario));
+    }
+    
+    @DeleteMapping("/{id_usuario}")
+    public void deleteUsuario (@PathVariable long id_usuario) {
+    	repository.deleteById(id_usuario);
+    }
 }
--- Imigrapp/Imigrapp/src/main/java/org/generation/Imigrapp/model/Tema.java
+++ Imigrapp/Imigrapp/src/main/java/org/generation/Imigrapp/model/Tema.java
@@ -1,30 +1,38 @@
 package org.generation.Imigrapp.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table (name="tb_tema")
+@Table(name = "tb_tema")
 public class Tema
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
-	@Size(min=5, max= 100)
+	@Size(min = 5, max = 100)
 	private String nomeTema;
-	
+
 	@NotNull
-	@Size(min=10, max=200)
-	
-	
+	@Size(min = 10, max = 200)
 	private String descricaoTema;
+
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem;
 
 	public long getId()
 	{
@@ -36,22 +44,24 @@ public class Tema
 		this.id = id;
 	}
 
-	public String getNomeTema() {
+	public String getNomeTema()
+	{
 		return nomeTema;
 	}
 
-	public void setNomeTema(String nomeTema) {
+	public void setNomeTema(String nomeTema)
+	{
 		this.nomeTema = nomeTema;
 	}
 
-	public String getDescricaoTema() {
+	public String getDescricaoTema()
+	{
 		return descricaoTema;
 	}
 
-	public void setDescricaoTema(String descricaoTema) {
+	public void setDescricaoTema(String descricaoTema)
+	{
 		this.descricaoTema = descricaoTema;
 	}
 
-	
-	
 }
