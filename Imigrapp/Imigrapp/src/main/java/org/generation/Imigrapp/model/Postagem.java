4a818f08720719b87e10f59797fef97998d64149
rafmenezes <rafael.mds92@gmail.com>
2021-08-04 14:09:48 -0300

    [TASK 6 - Postagem]

---
+++ Imigrapp/Imigrapp/src/main/java/org/generation/Imigrapp/model/Postagem.java
@@ -0,0 +1,85 @@
+package org.generation.Imigrapp.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name = "tb_postagem")
+public class Postagem 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_postagem;
+	
+	@NotBlank
+	private String campoPostagem;
+
+	private int compartilhamento;
+
+	private String comentario;
+	
+	private int interacao;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+	
+	public long getId_postagem() {
+		return id_postagem;
+	}
+
+	public void setId_postagem(long id_postagem) {
+		this.id_postagem = id_postagem;
+	}
+
+	public String getCampoPostagem() {
+		return campoPostagem;
+	}
+
+	public void setCampoPostagem(String campoPostagem) {
+		this.campoPostagem = campoPostagem;
+	}
+
+	public int getCompartilhamento() {
+		return compartilhamento;
+	}
+
+	public void setCompartilhamento(int compartilhamento) {
+		this.compartilhamento = compartilhamento;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+	public int getInteracao() {
+		return interacao;
+	}
+
+	public void setInteracao(int interacao) {
+		this.interacao = interacao;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	
+}
