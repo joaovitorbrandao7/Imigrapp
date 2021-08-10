371ba2ff108199257478f081fbb70c32aa40791e
joaovitorbrandao7 <joaovitor.brandao21@outlook.com>
2021-08-10 13:42:37 -0300

    [Task 7 - Adicionando UsuarioDTO e Usuario Service]

---
+++ Imigrapp/Imigrapp/src/main/java/org/generation/Imigrapp/model/UsuarioDTO.java
@@ -0,0 +1,79 @@
+package org.generation.Imigrapp.model;
+
+public class UsuarioDTO {
+	
+	private Long id_usuario;
+	private String nomeUsuario;
+	private String email;
+	private String senha;
+	private String tipoUsuario;
+	private String paisOrigem;
+	private String idioma;
+	private String token;
+	
+	public Long getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(Long id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	public String getPaisOrigem() {
+		return paisOrigem;
+	}
+
+	public void setPaisOrigem(String paisOrigem) {
+		this.paisOrigem = paisOrigem;
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
